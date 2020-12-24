294fe0948948bd9c53524c5975a840526f09f545
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-12-24 12:03:37 +0530

    get and post methods implemented

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
@@ -25,7 +26,8 @@ class ItemList(Resource):
     def get(self):
         return {'items': items}
 
+
 api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList,'/items')
+api.add_resource(ItemList, '/items')
 
 app.run(port=5000, debug=True)
