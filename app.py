a42fd1f6d9ede9c3c812a12700443a35ee969ad1
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-12-24 14:11:51 +0530

    put and delete implementation

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ items = []
 
 
 class Item(Resource):
-    # @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
@@ -30,6 +29,21 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
